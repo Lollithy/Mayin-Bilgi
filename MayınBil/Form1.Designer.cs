0c5098e43ae3912fb071c940312b51293d416d14
Zeynep Yoldaşlık <50807890+Lollithy@users.noreply.github.com>
2021-12-16 17:05:07 +0300

    Proje dosyası ekle.

---
+++ MayınBil/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace MayınBil
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.str = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.flowLayoutPanel1.BackgroundImage = global::MayınBil.Properties.Resources._37e56f4bd2f9caf9c7e98aedb5772bff;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 81);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(643, 590);
+            this.flowLayoutPanel1.TabIndex = 0;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // str
+            // 
+            this.str.BackColor = System.Drawing.Color.Red;
+            this.str.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.str.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.str.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.str.Location = new System.Drawing.Point(12, 17);
+            this.str.Name = "str";
+            this.str.Size = new System.Drawing.Size(118, 38);
+            this.str.TabIndex = 1;
+            this.str.Text = "Start";
+            this.str.UseVisualStyleBackColor = false;
+            this.str.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(437, 17);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 38);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(365, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Score :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(682, 703);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.str);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button str;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MayınBil/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayınBil
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       static int score = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.BackColor = Color.White;
+            str.BackColor = Color.Red;
+            bomba(15, 10);
+        }
+
+            void bomba (int bomb, int b)
+            {
+            flowLayoutPanel1.Width = b * 60;
+            flowLayoutPanel1.Height = b * 60;
+            flowLayoutPanel1.Controls.Clear();
+
+            int[] bombs = new int[bomb];
+            Random r = new Random();
+
+            for (int i = 0; i < bomb; i++) {
+
+              int chosen = r.Next(0, b * b);
+                if (bombs.Contains(chosen))
+                {
+                    i--;
+                    continue;
+                } bombs[i] = chosen;
+            }
+
+            for (int i = 0; i < b * b; i++)
+                {
+                    Button button = new Button();
+                button.Width = 50;
+                button.Height = 50;
+                button.BackColor = Color.Red;
+                button.FlatStyle = FlatStyle.Popup;
+                button.Tag = bombs.Contains(i);
+                button.Click += button_Click;
+                flowLayoutPanel1.Controls.Add(button);
+                }
+
+            }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button clicked = (Button)sender;
+            bool state = (bool)clicked.Tag;
+            if (state == true) 
+            {
+                  clicked.BackColor = Color.Red;
+                Questions question = new Questions();
+                question.Show();
+                this.Hide();
+                GameOver();
+            }
+
+            else 
+            {
+                clicked.BackColor = Color.Green;
+                score++;
+                textBox1.Text = score.ToString();
+            }
+
+        }
+
+        void Question()
+        {
+            
+        }
+        void GameOver()
+        {
+            foreach (Button item in flowLayoutPanel1.Controls)
+            {
+                bool state = (bool)item.Tag;
+                if (state)
+                {
+                    item.BackColor = Color.Red;
+                  
+                }
+                else
+                {
+                    item.BackColor = Color.Green;
+                }
+               
+                
+            }
+            textBox1.Clear();
+            MessageBox.Show("Skorunuz " + score + " tebrikler.");
+            score = 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
